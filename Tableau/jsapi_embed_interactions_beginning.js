413d179f6a2171c73323d813fa3b418680c4e457
snehakasetty224 <snehakasettysudarshan@gmail.com>
2016-12-02 21:04:57 -0800

    Add files via upload

---
+++ Tableau/jsapi_embed_interactions_beginning.js
@@ -0,0 +1,33 @@
+// Initialize the viz variable 
+var viz;
+window.onload= function() {
+// When the webpage has loaded, load the viz
+    var vizDiv = document.getElementById('myViz');
+     var vizURL ="https://10az.online.tableau.com/t/f22/views/SalesDashboard_0/SalesDashBoard?:embed=y&:showShareOptions=true&:display_count=no&:showVizHome=no";
+     var options ={
+      width: '900px',
+      height: '900px',
+      hideToolbar: true,
+      hideTabs: true 
+};
+ viz = new tableau.Viz(vizDiv, vizURL, options);
+    };
+
+// Switch the viz to the sheet specified
+function switchView(sheetName) {
+	var workbook =viz.getWorkbook();
+	workbook.activateSheetAsync(sheetName);
+
+}
+
+// Filter the specified dimension to the specified value(s)
+function show(filterName, values) {
+    var sheet =viz.getWorkbook().getActiveSheet();
+    sheet.applyFilterAsync(filterName,values,tableau.FilterUpdateType.REPLACE);
+}
+
+// Select the marks that have the specified value(s) for the specified dimension
+function selectMarks(filterName, values) {
+    var sheet =viz.getWorkbook().getActiveSheet();
+    sheet.applyFilterAsync(filterName,values,tableau.FilterUpdateType.REPLACE);
+}
